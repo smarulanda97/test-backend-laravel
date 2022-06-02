7fb36e4b722e8cecfc5599950e50a231c60d306c
Santiago Marulanda <santiagomm1997@gmail.com>
2022-06-03 01:16:51 +0200

    Very basic setup of package to work with JWT token
    php-open-source-saver/jwt-auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthPostRequest;
+
+class AuthController extends Controller
+{
+    /**
+     * AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => 'login']);
+    }
+
+    /**
+     * Returns JWT if credentials are valid.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(AuthPostRequest $request) {
+        if (!($token = auth()->attempt($request->validated()))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->responseWithToken($token);
+    }
+
+    /**
+     * Logs the user out
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout() {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Returns the authenticated user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me() {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Refresh JWT token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->responseWithToken(auth()->refresh());
+    }
+
+    /**
+     * Returns response object with the given token
+     *
+     * @param string $token
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function responseWithToken(string $token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api', 'prefix' =>'auth'], function () {
+    Route::post('/me', [AuthController::class, 'me']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
 });
